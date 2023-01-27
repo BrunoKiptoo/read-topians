38c9e1d0dbe5dffe8ab1649c2958543588b5c3e4
LinexMugambi <117725110+LinexMugambi@users.noreply.github.com>
2023-01-27 09:05:23 +0300

    Update Navbar.js
    added some code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-//navbar
+
 function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
